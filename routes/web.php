9a6481655c17d6e31439e7a50c5462030bb47b5a
Raka Haikal <rakahaikal924@gmail.com>
2024-12-13 17:58:20 +0700

    Map all of the questions in resource into QuestionSummary component

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,12 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return QuestionResource::collection(
+    $questions = QuestionResource::collection(
         Question::with('user')->latest()->paginate(15)
     );
 
     return inertia('Questions/Index', [
-        'questions' => [
-            ['id' => 1, 'title' => 'Question 1'],
-            ['id' => 2, 'title' => 'Question 2'],
-            ['id' => 3, 'title' => 'Question 3'],
-            ['id' => 4, 'title' => 'Question 4'],
-        ]
+        'questions' => $questions
     ]);
 })->name('questions.index');
 
